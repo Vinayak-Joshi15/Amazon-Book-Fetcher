b51598e7376b1ceb6738a5d6eefa5a51d0f555e4
Vinayak Joshi <139743089+Vinayak-Joshi15@users.noreply.github.com>
2023-12-25 13:44:17 +0530

    Add files via upload

---
+++ Amazon_link_giver.py
@@ -0,0 +1,26 @@
+import webbrowser
+
+t = []
+url = ""
+buffer = ""
+
+name = str(input("enter the name of the book: "))
+t.append(name)
+
+author = str(input("enter the name of the book: "))
+t.append(author)
+
+genre = str(input("enter the primary genre of the book: "))
+t.append(genre)
+
+book_data = tuple(t)
+
+print(book_data)
+
+str1 = book_data[0].split()
+for i in range(len(str1)):
+    buffer = buffer + str1[i] + "+"
+
+url = "https://www.amazon.in/s?k=" + buffer + "&i=stripbooks"
+print("url: ",url)
+webbrowser.open(url, new=0)
